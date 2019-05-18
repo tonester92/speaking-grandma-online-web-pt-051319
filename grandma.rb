00636b7689564c7421b31fd56373df6e7f42a9fc
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-18 21:16:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,13 +13,3 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(phrase = "I LOVE YOU GRANDMA")
-  if phrase != phrase.uppercase
-    puts "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-end
-end
-speak_to_grandma
